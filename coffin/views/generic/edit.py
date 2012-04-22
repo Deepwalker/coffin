5313abe5dbae1b32772c8de236e9a667e4822363
Mihail Krivushin <krivushinme@gmail.com>
2012-04-23 02:44:58 +0400

    Added FormView

--- coffin/views/generic/edit.py
+++ coffin/views/generic/edit.py
@@ -1,8 +1,10 @@
-from django.views.generic.edit import CreateView as _CreateView, UpdateView as _UpdateView, DeleteView as _DeleteView
+from django.views.generic import edit
 from coffin.views.decorators import template_response
 
 __all__ = ['CreateView', 'UpdateView', 'DeleteView']
 
-CreateView, UpdateView, DeleteView = map(template_response,
-        (_CreateView, _UpdateView, _DeleteView)) 
+CreateView, UpdateView, DeleteView, FormView = [
+        template_response(getattr(edit, view)) for view in (
+        'CreateView', 'UpdateView', 'DeleteView', 'FormView'
+    )]
 
